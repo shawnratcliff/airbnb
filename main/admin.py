2d6f3ced5e6417fe35ea0b3254d8ba8c305d92f4
Shawn Ratcliff <shawn.ratcliff@gmail.com>
2017-01-17 02:51:52 -0800

    Registering Neighborhood model in geospatial admin interface.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Neighborhood
 
-# Register your models here.
+admin.site.register(Neighborhood, admin.OSMGeoAdmin)
+
