086b244fbb0692f170e7b6de1791154d6773a76b
Shawn Ratcliff <shawn.ratcliff@gmail.com>
2017-01-17 02:49:49 -0800

    Updating Neighborhood model definition

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    mpoly = models.MultiPolygonField()
+    def __str__(self):
+        return self.name
+
+
+
